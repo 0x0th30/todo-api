9892a510f77bbef9bf121538fe900d8fbcc69d6d
theo silva <theodorotheodore@gmail.com>
2023-12-05 14:07:50 -0300

    refactor: change sufix from "middleware" to "router"

--- app/src/api/routes.py
+++ app/src/api/routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
-from src.use_cases.create import create_middleware
-from src.use_cases.read import read_middleware
-from src.use_cases.update import update_middleware
-from src.use_cases.delete import delete_middleware
+from src.use_cases.create import create_router
+from src.use_cases.read import read_router
+from src.use_cases.update import update_router
+from src.use_cases.delete import delete_router
 
 router = APIRouter()
-router.include_router(create_middleware.router, prefix="", tags=[])
-router.include_router(read_middleware.router, prefix="", tags=[])
-router.include_router(update_middleware.router, prefix="", tags=[])
-router.include_router(delete_middleware.router, prefix="", tags=[])
+router.include_router(create_router.router, prefix="", tags=[])
+router.include_router(read_router.router, prefix="", tags=[])
+router.include_router(update_router.router, prefix="", tags=[])
+router.include_router(delete_router.router, prefix="", tags=[])
